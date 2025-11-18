f1b3115e4ea2ce2e99b5f5b7962b9db2cbfcb87a
w0 <33212583+w0@users.noreply.github.com>
2025-11-17 21:03:14 -0600

    connect to mq server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	conStr := "amqp://guest:guest@localhost:5672/"
+
+	conn, err := amqp.Dial(conStr)
+	failOnError(err, "Failed to connect to rabbit mq")
+	defer conn.Close()
+
+	log.Println("Connection successful")
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+
+	log.Println("os interrupt recieved. shutting down..")
+
 }
