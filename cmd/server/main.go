674feb1160205dbc78ae57f340d0dede0853c65d
w0 <33212583+w0@users.noreply.github.com>
2025-11-18 19:13:59 -0600

    publish json to exchange

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -24,7 +26,19 @@ func main() {
 	failOnError(err, "Failed to connect to rabbit mq")
 	defer conn.Close()
 
-	log.Println("Connection successful")
+	log.Println("connection successful")
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to create channel")
+
+	err = pubsub.PublishJSON(ch, string(routing.ExchangePerilDirect), string(routing.PauseKey),
+		routing.PlayingState{
+			IsPaused: true,
+		})
+
+	failOnError(err, "Failed to publish message")
+
+	log.Println("message published")
 
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt)
---
+++ internal/pubsub/publish.go
@@ -0,0 +1,22 @@
+package pubsub
+
+import (
+	"context"
+	"encoding/json"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
+	json, err := json.Marshal(val)
+	if err != nil {
+		return err
+	}
+
+	err = ch.PublishWithContext(context.Background(), exchange, key, false, false, amqp.Publishing{
+		Body:        json,
+		ContentType: "application/json",
+	})
+
+	return err
+}
